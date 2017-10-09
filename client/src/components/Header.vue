e1025f7681e77457feea87b7ae04d0825e776b47
Brian Wuollet <bwuollet@is-mbp-bwuollet3.in.ionicsecurity.com>
2017-10-09 08:51:41 -0400

    wired login page

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -17,6 +17,11 @@
 
         <v-toolbar-items>
             <v-btn flat dark
+                @click="navigateTo({name: 'login'})">
+                Login
+            </v-btn>
+
+            <v-btn flat dark
                 @click="navigateTo({name: 'register'})">
                 Sign Up
             </v-btn>
---
+++ client/src/components/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-layout column>
+        <v-flex xs6 offset-xs3>
+          <div class="white elevation-2">
+            <v-toolbar flat dense class="red darken-3" dark>
+              <v-toolbar-title>Login</v-toolbar-title>
+            </v-toolbar>
+            
+            <div class="pl-4 pr-4 pt-2 pb-2">
+              <v-text-field 
+                label="email"
+                v-model="email"
+              ></v-text-field>
+              <v-text-field 
+                label="password"
+                v-model="password"
+              ></v-text-field>
+              <br>
+              <div class="error" v-html="error" />
+              <br>
+              <v-btn 
+                dark
+                class="red darken-3"
+                @click="login">
+                Login
+              </v-btn>
+            </div>
+          </div>
+        </v-flex>
+      </v-layout>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async login () {
+      try {
+        await AuthenticationService.login({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
